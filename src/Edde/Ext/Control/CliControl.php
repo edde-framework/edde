1c3314367b9582e3e4b099476636fe7b7745dd4b
Marek Hanzal <marek.hanzal@flairoo.com>
2017-10-14 16:19:22 +0200

    - added abort method in cli control

--- src/Edde/Ext/Control/CliControl.php
+++ src/Edde/Ext/Control/CliControl.php
@@ -1,8 +1,22 @@
 <?php
 	namespace Edde\Ext\Control;
 
+		use Edde\Api\Application\Exception\AbortException;
+
 		/**
 		 * Control used for a command line content rendering.
 		 */
 		trait CliControl {
+			/**
+			 * just "nicier" way, how to send an abort exception
+			 *
+			 * @param string          $message
+			 * @param int             $code
+			 * @param \Throwable|null $throwable
+			 *
+			 * @throws AbortException
+			 */
+			public function abort(string $message, int $code = -1, \Throwable $throwable = null) {
+				throw new AbortException($message, $code, $throwable);
+			}
 		}
