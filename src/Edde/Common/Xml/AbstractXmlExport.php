49354f260016bb67744ec51c692a6c5e7e688395
Marek Hanzal <marek.hanzal@flairoo.com>
2017-10-15 17:22:52 +0200

    - added xml exporter

---
+++ src/Edde/Common/Xml/AbstractXmlExport.php
@@ -0,0 +1,29 @@
+<?php
+	declare(strict_types=1);
+	namespace Edde\Common\Xml;
+
+		use Edde\Api\File\IFile;
+		use Edde\Api\Xml\IXmlExport;
+		use Edde\Common\Object\Object;
+		use Iterator;
+
+		abstract class AbstractXmlExport extends Object implements IXmlExport {
+			/**
+			 * @inheritdoc
+			 */
+			public function export(Iterator $iterator, IFile $file): IFile {
+				$file->openForWrite();
+				$file->write($this->string($iterator));
+				$file->close();
+				return $file;
+			}
+
+			/**
+			 * @inheritdoc
+			 */
+			public function string(Iterator $iterator): string {
+				ob_start();
+				$this->node($iterator);
+				return ob_get_clean();
+			}
+		}
