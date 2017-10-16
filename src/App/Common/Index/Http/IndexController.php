89a92bc73e1cccfe451ba4dc72fc3916995de44a
Marek Hanzal <marek.hanzal@flairoo.com>
2017-10-16 14:45:54 +0200

    - removed default router - clinet will be fully standalone ((almost) no server-side required)

--- src/App/Common/Index/Http/IndexController.php
+++ src/App/Common/Index/Http/IndexController.php
@@ -3,14 +3,11 @@
 	namespace App\Common\Index\Http;
 
 		use App\Common\Index\AbstractIndexController;
-		use Edde\Api\Assets\Inject\AssetsDirectory;
 		use Edde\Ext\Control\HttpController;
 
 		class IndexController extends AbstractIndexController {
 			use HttpController;
-			use AssetsDirectory;
 
 			public function actionIndex() {
-				$this->html($this->assetsDirectory->directory('templates')->file('index.html'));
 			}
 		}
--- src/App/Common/Router/RouterServiceConfigurator.php
+++ src/App/Common/Router/RouterServiceConfigurator.php
@@ -7,9 +7,7 @@
 		use Edde\Api\Container\Inject\Container;
 		use Edde\Api\Router\IRouterService;
 		use Edde\Common\Config\AbstractConfigurator;
-		use Edde\Common\Element\Message;
 		use Edde\Ext\Router\ProtocolServiceRouter;
-		use Edde\Ext\Router\StaticRouter;
 
 		class RouterServiceConfigurator extends AbstractConfigurator {
 			use Container;
@@ -22,23 +20,12 @@
 			 */
 			public function configure($instance) {
 				parent::configure($instance);
-				$controller = 'Index\Http\IndexController';
-				$method = 'actionIndex';
 				$instance->registerRouterList([
 					/**
 					 * because whole application is built around The Protocol implementation, also router is bound to one of
 					 * protocol's services to check, if the system is able to handle current request
 					 */
 					$this->container->create(ProtocolServiceRouter::class, [], __METHOD__),
-					/**
-					 * last router is considered as a default
-					 */
-					$this->container->create(StaticRouter::class, [
-						(new Message($controller . '::' . $method))->mergeMetaList([
-							'::class'  => $controller,
-							'::method' => $method,
-						]),
-					], __METHOD__),
 				]);
 			}
 		}
