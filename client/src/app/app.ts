bdf90ca4984e1225ac7bd4ebc2c57673beb576d3
Marek Hanzal <marek.hanzal@flairoo.com>
2017-10-17 08:11:45 +0200

    - doc

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -7,4 +7,8 @@ import {LoaderView} from "./loader/LoaderView";
 e3.RequestService();
 e3.ProtocolService();
 
+/**
+ * hardcoded loader view will display initial loader before everything other will be available;
+ * attachTo also do the rendering
+ */
 new LoaderView().attachTo(e3.el(document.body));
