ede1e8d0870404036efa5902c662c40e6278434a
Marek Hanzal <marek.hanzal@flairoo.com>
2017-10-18 17:00:36 +0200

    - preparing markup of a registration form

--- client/src/app/register/RegisterView.ts
+++ client/src/app/register/RegisterView.ts
@@ -23,7 +23,51 @@ export class RegisterView extends AbstractControl {
 						</div>
 					</div>
 				</section>
-			`)
+			`),
+			e3.html(`
+				<section class="section">
+					<div class="columns">
+						<div class="column is-offset-one-quarter is-half">
+							<div class="field">
+								<p class="control has-icons-left has-icons-right">
+									<input class="input" type="email" placeholder="Email">
+									<span class="icon is-small is-left">
+										<i class="fa fa-envelope"></i>
+									</span>
+									<span class="icon is-small is-right"><i class="fa fa-check"></i></span>
+								</p>
+							        <p class="help">later you will use this to login</p>
+							</div>
+							<div class="field">
+								<p class="control has-icons-left">
+									<input class="input" type="password" placeholder="Password">
+									<span class="icon is-small is-left">
+										<i class="fa fa-lock"></i>
+									</span>
+								</p>
+								<p class="help">think about something long and safe</p>
+							</div>
+							<div class="field">
+								<p class="control has-icons-left has-icons-right">
+									<input class="input" type="password" placeholder="Type again">
+									<span class="icon is-small is-left">
+										<i class="fa fa-lock"></i>
+									</span>
+									<span class="icon is-small is-right"><i class="fa fa-check"></i></span>
+								</p>
+							</div>
+							<div class="field is-grouped is-grouped-right">
+								<div class="control">
+									<button class="button is-text">Cancel</button>
+								</div>
+								<div class="control">
+									<button class="button is-primary">Register</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			`),
 		]);
 	}
 }
