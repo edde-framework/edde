2b585e6211f21b7c905ddfcc37f9c9a7c61cf5cd
Marek Hanzal <marek.hanzal@flairoo.com>
2017-10-19 18:33:37 +0200

    - WhereThan fragment looks better

--- src/Edde/Common/Query/Fragment/WhereThanFragment.php
+++ src/Edde/Common/Query/Fragment/WhereThanFragment.php
@@ -1,10 +1,21 @@
 <?php
 	namespace Edde\Common\Query\Fragment;
 
+		use Edde\Common\Node\Node;
+
 		class WhereThanFragment extends AbstractFragment {
-			public function than(string $name): WhereRelationFragment {
+			public function than($value): WhereRelationFragment {
+				/** @noinspection PhpUnhandledExceptionInspection */
+				$this->node->setAttribute('than', $id = (sha1(random_bytes(64) . microtime(true))));
+				$this->root->addNode(new Node('parameter', null, [
+					'name'  => $id,
+					'value' => $value,
+				]));
+				return new WhereRelationFragment($this->root, $this->node);
 			}
 
 			public function thanColumn(string $name): WhereRelationFragment {
+				$this->node->setAttribute('than-column', $name);
+				return new WhereRelationFragment($this->root, $this->node);
 			}
 		}
