a80fc92f8db501caf633e85044e6c8612211a2ab
Marek Hanzal <marek.hanzal@flairoo.com>
2017-10-19 18:47:17 +0200

    - where relation experimentally done

--- src/Edde/Common/Query/Fragment/WhereRelationFragment.php
+++ src/Edde/Common/Query/Fragment/WhereRelationFragment.php
@@ -4,12 +4,15 @@
 		class WhereRelationFragment extends AbstractFragment {
 			public function and (): WhereFragment {
 				$this->node->setAttribute('relation', 'and');
+				return new WhereFragment($this->root, $this->node->getParent());
 			}
 
 			public function or (): WhereFragment {
 				$this->node->setAttribute('relation', 'and');
+				return new WhereFragment($this->root, $this->node->getParent());
 			}
 
 			public function end(): WhereRelationFragment {
+				return new WhereRelationFragment($this->root, $this->node->getParent()->getParent());
 			}
 		}
