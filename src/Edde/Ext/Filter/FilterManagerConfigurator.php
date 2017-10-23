676189642f1d5bff1e6bbcc0eaec91dd00d394ec
Marek Hanzal <marek.hanzal@x32.cz>
2017-10-23 23:51:32 +0200

    - first filter is there!

--- src/Edde/Ext/Filter/FilterManagerConfigurator.php
+++ src/Edde/Ext/Filter/FilterManagerConfigurator.php
@@ -1,17 +1,26 @@
 <?php
 	namespace Edde\Ext\Filter;
 
+		use Edde\Api\Container\Exception\ContainerException;
+		use Edde\Api\Container\Exception\FactoryException;
 		use Edde\Api\Container\Inject\Container;
 		use Edde\Api\Filter\IFilterManager;
 		use Edde\Common\Config\AbstractConfigurator;
+		use Edde\Common\Filter\ToBoolFilter;
 
 		class FilterManagerConfigurator extends AbstractConfigurator {
 			use Container;
 
 			/**
 			 * @param IFilterManager $instance
+			 *
+			 * @throws ContainerException
+			 * @throws FactoryException
 			 */
 			public function configure($instance) {
 				parent::configure($instance);
+				$instance->registerFilterList([
+					'bool' => $this->container->create(ToBoolFilter::class, [], __METHOD__),
+				]);
 			}
 		}
