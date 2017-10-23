fc34f8b799bb7f2e945967cb521ace3a9988e745
Marek Hanzal <marek.hanzal@x32.cz>
2017-10-23 23:48:53 +0200

    - looks like filter has basically same code as generator...

--- src/Edde/Api/Filter/IFilterManager.php
+++ src/Edde/Api/Filter/IFilterManager.php
@@ -4,4 +4,22 @@
 		use Edde\Api\Config\IConfigurable;
 
 		interface IFilterManager extends IConfigurable {
+			/**
+			 * register the given filter
+			 *
+			 * @param string  $name
+			 * @param IFilter $filter
+			 *
+			 * @return IFilterManager
+			 */
+			public function registerFilter(string $name, IFilter $filter): IFilterManager;
+
+			/**
+			 * register list of filters
+			 *
+			 * @param IFilter[] $filterList
+			 *
+			 * @return IFilterManager
+			 */
+			public function registerFilterList(array $filterList): IFilterManager;
 		}
--- src/Edde/Common/Filter/FilterManager.php
+++ src/Edde/Common/Filter/FilterManager.php
@@ -1,8 +1,55 @@
 <?php
 	namespace Edde\Common\Filter;
 
+		use Edde\Api\Filter\Exception\UnknownFilterException;
+		use Edde\Api\Filter\IFilter;
 		use Edde\Api\Filter\IFilterManager;
 		use Edde\Common\Object\Object;
 
 		class FilterManager extends Object implements IFilterManager {
+			/**
+			 * @var IFilter[]
+			 */
+			protected $filterList = [];
+
+			/**
+			 * @inheritdoc
+			 */
+			public function registerFilter(string $name, IFilter $filter): IFilterManager {
+				$this->filterList[$name] = $filter;
+				return $this;
+			}
+
+			/**
+			 * @inheritdoc
+			 */
+			public function registerFilterList(array $filterList): IFilterManager {
+				foreach ($filterList as $name => $filter) {
+					$this->registerFilter($name, $filter);
+				}
+				return $this;
+			}
+
+			/**
+			 * @inheritdoc
+			 */
+			public function getFilter(string $name): IFilter {
+				if (isset($this->filterList[$name]) === false) {
+					throw new UnknownFilterException(sprintf('Requested unknown filter [%s].', $name));
+				}
+				return $this->filterList[$name];
+			}
+
+			/**
+			 * @inheritdoc
+			 */
+			public function generate(array $source): array {
+				$result = $source;
+				foreach ($source as $k => $v) {
+					if (isset($this->filterList[$k])) {
+						$result[$k] = $this->filterList[$k]->filter($v);
+					}
+				}
+				return $result;
+			}
 		}
