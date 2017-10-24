01b62bc8d21a8dd1a909caa21d2d9962095572cd
Marek Hanzal <marek.hanzal@x32.cz>
2017-10-24 14:58:49 +0200

    - so how to build relations in schema?

--- src/Edde/Ext/Schema/SchemaReflectionLoader.php
+++ src/Edde/Ext/Schema/SchemaReflectionLoader.php
@@ -36,42 +36,45 @@
 					$reflectionClass = new \ReflectionClass($name);
 					$schema = Schema::create($name);
 					foreach ($reflectionClass->getMethods() as $reflectionMethod) {
-						if (($doc = $reflectionMethod->getDocComment()) === false || strpos($doc, '@schema') === false) {
+						if (($doc = $reflectionMethod->getDocComment()) === false) {
 							continue;
-						}
-						$attr = $this->stringUtils->match($doc, '~@schema\s*(?<attr>.*?)[\n\r]~sm', true);
-						$attr = $attr['attr'] ?? '';
-						$property = $schema->property($name = $reflectionMethod->getName());
-						$property->type('string');
-						if (strpos($attr, 'unique') !== false) {
-							$property->unique();
-						}
-						if (strpos($attr, 'primary') !== false) {
-							$property->primary();
-							$property->generator($name);
-						}
-						if (($type = $reflectionMethod->getReturnType()) !== null) {
-							$property->type($type->getName());
-							$property->required($type->allowsNull() === false);
-						}
-						switch ($type = $property->getType()) {
-							case 'float':
-							case 'int':
-							case 'bool':
-							case 'datetime':
-							case \DateTime::class:
-								$property->filter($type);
-								$property->sanitizer($type);
-								break;
-						}
-						if (($generator = $this->stringUtils->match($doc, '~@generator\s*(?<value>.*?)[\n\r]~sm', true)) !== null) {
-							$property->generator(trim($generator['value']));
-						}
-						if (($filter = $this->stringUtils->match($doc, '~@filter\s*(?<value>.*?)[\n\r]~sm', true)) !== null) {
-							$property->filter(trim($filter['value']));
-						}
-						if (($sanitizer = $this->stringUtils->match($doc, '~@sanitizer\s*(?<value>.*?)[\n\r]~sm', true)) !== null) {
-							$property->sanitizer(trim($sanitizer['value']));
+						} else if (strpos($doc, '@schema') !== false) {
+							$attr = $this->stringUtils->match($doc, '~@schema\s*(?<attr>.*?)[\n\r]~sm', true);
+							$attr = $attr['attr'] ?? '';
+							$property = $schema->property($name = $reflectionMethod->getName());
+							$property->type('string');
+							if (strpos($attr, 'unique') !== false) {
+								$property->unique();
+							}
+							if (strpos($attr, 'primary') !== false) {
+								$property->primary();
+								$property->generator($name);
+							}
+							if (($type = $reflectionMethod->getReturnType()) !== null) {
+								$property->type($type->getName());
+								$property->required($type->allowsNull() === false);
+							}
+							switch ($type = $property->getType()) {
+								case 'float':
+								case 'int':
+								case 'bool':
+								case 'datetime':
+								case \DateTime::class:
+									$property->filter($type);
+									$property->sanitizer($type);
+									break;
+							}
+							if (($generator = $this->stringUtils->match($doc, '~@generator\s*(?<value>.*?)[\n\r]~sm', true)) !== null) {
+								$property->generator(trim($generator['value']));
+							}
+							if (($filter = $this->stringUtils->match($doc, '~@filter\s*(?<value>.*?)[\n\r]~sm', true)) !== null) {
+								$property->filter(trim($filter['value']));
+							}
+							if (($sanitizer = $this->stringUtils->match($doc, '~@sanitizer\s*(?<value>.*?)[\n\r]~sm', true)) !== null) {
+								$property->sanitizer(trim($sanitizer['value']));
+							}
+						} else if (strpos($doc, '@relation') !== false) {
+							$attr = $this->stringUtils->match($doc, '~@relation\s*(?<attr>.*?)[\n\r]~sm', true);
 						}
 					}
 					return $this->schemaList[$schema->getName()] = $schema;
