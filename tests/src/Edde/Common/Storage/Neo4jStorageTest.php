5da71a16f773b7a885dfdf4231cf0a199a04df32
Marek Hanzal <marek.hanzal@x32.cz>
2017-10-26 14:17:48 +0200

    - so play first with neo4j

--- tests/src/Edde/Common/Storage/Neo4jStorageTest.php
+++ tests/src/Edde/Common/Storage/Neo4jStorageTest.php
@@ -4,6 +4,7 @@
 		use Edde\Api\Storage\Inject\Storage;
 		use Edde\Api\Storage\IStorage;
 		use Edde\Common\Container\Factory\ClassFactory;
+		use Edde\Common\Query\NativeQuery;
 		use Edde\Ext\Container\ContainerFactory;
 		use Edde\Ext\Storage\Neo4jStorage;
 		use Edde\Ext\Test\TestCase;
@@ -12,6 +13,7 @@
 			use Storage;
 
 			public function testPrepareDatabase() {
+				$this->storage->native(new NativeQuery('MATCH (n) DETACH DELETE n'));
 			}
 
 			protected function setUp() {
