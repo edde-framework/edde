8046ccae96efe6d5bd3f6cec47184e7869411a69
Marek Hanzal <marek.hanzal@x32.cz>
2017-10-26 16:57:29 +0200

    - prepared neo4j tests

--- src/Edde/Ext/Driver/Graph/Neo4j/Neo4jDriver.php
+++ src/Edde/Ext/Driver/Graph/Neo4j/Neo4jDriver.php
@@ -4,12 +4,29 @@
 		use Edde\Api\Driver\IDriver;
 		use Edde\Api\Query\INativeQuery;
 		use Edde\Common\Driver\AbstractDriver;
+		use GraphAware\Bolt\GraphDatabase;
+		use GraphAware\Bolt\Protocol\SessionInterface;
 
 		class Neo4jDriver extends AbstractDriver {
+			/** @var string */
+			protected $url;
+			/**
+			 * @var SessionInterface
+			 */
+			protected $session;
+
+			/**
+			 * @param string $url
+			 */
+			public function __construct(string $url) {
+				$this->url = $url;
+			}
+
 			/**
 			 * @inheritdoc
 			 */
 			public function execute(INativeQuery $nativeQuery) {
+				return $this->session->run($nativeQuery->getQuery(), $nativeQuery->getParameterList());
 			}
 
 			/**
@@ -31,5 +48,10 @@
 			 */
 			public function rollback(): IDriver {
 				return $this;
+			}
+
+			protected function handleSetup(): void {
+				parent::handleSetup();
+				$this->session = GraphDatabase::driver($this->url)->session();
 			}
 		}
--- tests/src/Edde/Common/Storage/Neo4jStorageTest.php
+++ tests/src/Edde/Common/Storage/Neo4jStorageTest.php
@@ -1,14 +1,14 @@
 <?php
-	namespace Edde\Common\Storage\Neo4j;
+	namespace Edde\Common\Storage;
 
 		use Edde\Api\Container\Exception\ContainerException;
 		use Edde\Api\Container\Exception\FactoryException;
+		use Edde\Api\Driver\IDriver;
 		use Edde\Api\Storage\Inject\Storage;
-		use Edde\Api\Storage\IStorage;
 		use Edde\Common\Container\Factory\ClassFactory;
 		use Edde\Common\Query\NativeQuery;
 		use Edde\Ext\Container\ContainerFactory;
-		use Edde\Ext\Storage\Neo4j\Neo4jStorage;
+		use Edde\Ext\Driver\Graph\Neo4j\Neo4jDriver;
 		use Edde\Ext\Test\TestCase;
 
 		class Neo4jStorageTest extends TestCase {
@@ -25,7 +25,7 @@
 			 */
 			protected function setUp() {
 				ContainerFactory::inject($this, [
-					IStorage::class => ContainerFactory::instance(Neo4jStorage::class, []),
+					IDriver::class => ContainerFactory::instance(Neo4jDriver::class, ['bolt://172.17.0.1']),
 					new ClassFactory(),
 				]);
 			}
