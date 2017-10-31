ee182ac3c17b2d5cbe60e98cfadb8530e49c7139
Marek Hanzal <marek.hanzal@x32.cz>
2017-10-31 08:58:27 +0100

    - update idea of schema relation

--- src/Edde/Api/Schema/IRelation.php
+++ src/Edde/Api/Schema/IRelation.php
@@ -2,4 +2,26 @@
 	namespace Edde\Api\Schema;
 
 		interface IRelation {
+			/**
+			 * get relation schema
+			 *
+			 * @return ISchema
+			 */
+			public function getSchema(): ISchema;
+
+			/**
+			 * return source link; link should be connected to relation schema and to
+			 * a property of relation schema
+			 *
+			 * @return ILink
+			 */
+			public function getSourceLink(): ILink;
+
+			/**
+			 * return target link; link should be connected to relation schema and to
+			 * a property of relation schema
+			 *
+			 * @return ILink
+			 */
+			public function getTargetLink(): ILink;
 		}
---
+++ src/Edde/Common/Schema/Relation.php
@@ -0,0 +1,49 @@
+<?php
+	namespace Edde\Common\Schema;
+
+		use Edde\Api\Schema\ILink;
+		use Edde\Api\Schema\IRelation;
+		use Edde\Api\Schema\ISchema;
+		use Edde\Common\Object\Object;
+
+		class Relation extends Object implements IRelation {
+			/**
+			 * @var ISchema
+			 */
+			protected $schema;
+			/**
+			 * @var ILink
+			 */
+			protected $sourceLink;
+			/**
+			 * @var ILink
+			 */
+			protected $targetLink;
+
+			public function __construct(ISchema $schema, ILink $sourceLink, ILink $targetLink) {
+				$this->schema = $schema;
+				$this->sourceLink = $sourceLink;
+				$this->targetLink = $targetLink;
+			}
+
+			/**
+			 * @inheritdoc
+			 */
+			public function getSchema(): ISchema {
+				return $this->schema;
+			}
+
+			/**
+			 * @inheritdoc
+			 */
+			public function getSourceLink(): ILink {
+				return $this->sourceLink;
+			}
+
+			/**
+			 * @inheritdoc
+			 */
+			public function getTargetLink(): ILink {
+				return $this->targetLink;
+			}
+		}
