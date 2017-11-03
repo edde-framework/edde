cf41356573a2fbf41a8ba6f85f6717fdc6ee3fcd
Marek Hanzal <marek.hanzal@x32.cz>
2017-11-03 10:17:44 +0100

    - preparing fragments in a driver

--- src/Edde/Common/Driver/AbstractDriver.php
+++ src/Edde/Common/Driver/AbstractDriver.php
@@ -5,8 +5,15 @@
 		use Edde\Api\Driver\IDriver;
 		use Edde\Api\Query\IQuery;
 		use Edde\Common\Object\Object;
+		use ReflectionClass;
+		use ReflectionMethod;
 
 		abstract class AbstractDriver extends Object implements IDriver {
+			/**
+			 * @var callable[]
+			 */
+			protected $fragmentList;
+
 			/**
 			 * @inheritdoc
 			 */
@@ -15,5 +22,15 @@
 
 			protected function handleSetup(): void {
 				parent::handleSetup();
+				$reflectionClass = new ReflectionClass($this);
+				foreach ($reflectionClass->getMethods(ReflectionMethod::IS_PROTECTED) as $reflectionMethod) {
+					if (strpos($name = $reflectionMethod->getName(), 'fragment') === false || strlen($name) <= 8) {
+						continue;
+					}
+					$this->fragmentList[$this->stringUtils->recamel(substr($name, 8))] = [
+						$this,
+						$name,
+					];
+				}
 			}
 		}
