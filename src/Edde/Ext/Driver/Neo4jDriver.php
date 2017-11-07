00e83936759180354e58ac48de51d8b47fa162fe
Marek Hanzal <marek.hanzal@x32.cz>
2017-11-07 10:52:52 +0100

    - removed some obsolete stuff

--- src/Edde/Ext/Driver/Neo4jDriver.php
+++ src/Edde/Ext/Driver/Neo4jDriver.php
@@ -7,14 +7,9 @@
 		use Edde\Api\Driver\IDriver;
 		use Edde\Api\Query\Fragment\IWhere;
 		use Edde\Api\Query\ICrateSchemaQuery;
-		use Edde\Api\Query\ICreateRelationQuery;
-		use Edde\Api\Query\IInsertQuery;
 		use Edde\Api\Query\INativeQuery;
 		use Edde\Api\Query\ISelectQuery;
 		use Edde\Api\Query\ITransactionQuery;
-		use Edde\Api\Query\IUpdateLinkQuery;
-		use Edde\Api\Query\IUpdateQuery;
-		use Edde\Api\Query\IUpdateRelationQuery;
 		use Edde\Api\Storage\Exception\DuplicateEntryException;
 		use Edde\Api\Storage\Exception\NullValueException;
 		use Edde\Common\Driver\AbstractDriver;
@@ -188,130 +183,6 @@
 				$this->native($cypher, $parameterList);
 			}
 
-			protected function executeEntityQuery() {
-				/**
-				 * create or update, with M:N relations maintained
-				 *
-				 * - it's not possible to remove relation and create a new one to different node
-				 */
-				/** @noinspection PhpExpressionResultUnusedInspection */
-				/** @lang Cypher */
-				<<< 'CYPHER'
-// new node creation using GUID as a node name
-MERGE (`foo-1`:foo {guid: 'foo1'}) SET `foo-1` = {guid: 'foo1', name: 'foo-1'}
-MERGE (`foo-2`:foo {guid: 'foo2'}) SET `foo-2` = {guid: 'foo2', name: 'foo-2'}
-MERGE (`foo-3`:foo {guid: 'foo3'}) SET `foo-3` = {guid: 'foo3', name: 'foo-3'}
-MERGE (`bar-1`:bar {guid: 'bar1'}) SET `bar-1` = {guid: 'bar1', name: 'bar-1'}
-MERGE (`bar-2`:bar {guid: 'bar2'}) SET `bar-2` = {guid: 'bar2', name: 'bar-2'}
-MERGE (`subBar-1`:`sub-bar` {guid: 'subBar-1'}) SET	`subBar-1` = {guid: 'subBar-1', label: 'subBar #1'}
-MERGE (`subBar-2`:`sub-bar` {guid: 'subBar-2'}) SET	`subBar-2` = {guid: 'subBar-2', label: 'subBar #2'}
-
-//m:n relations
-MERGE (`foo-1`)-[:`foo-bar`]->(`bar-1`)
-MERGE (`foo-2`)-[:`foo-bar`]->(`bar-1`)
-MERGE (`foo-3`)-[:`foo-bar`]->(`bar-2`)
-
-// maintain 1:n relations - this is not working
-//WITH `bar-2` MATCH (`bar-2-2` {guid: `bar-2`.guid})-[r:`sub-bar`]->(:`sub-bar`) DELETE r
-//MERGE (`bar-2`)-[:`sub-bar`]->(`subBar-2`)
-//WITH `bar-2` MATCH (`bar-2-2` {guid: `bar-2`.guid})-[r:`sub-bar`]->(:`sub-bar`) DELETE r
-//MERGE (`bar-2`)-[:`sub-bar`]->(`subBar-1`)
-CYPHER;
-			}
-
-			/**
-			 * @param IInsertQuery $insertQuery
-			 *
-			 * @throws \Throwable
-			 */
-			protected function executeInsertQuery(IInsertQuery $insertQuery) {
-				$this->native('CREATE (n:' . $this->delimite(($schema = $insertQuery->getSchema())->getRealName()) . ' $set)', [
-					'set' => $this->schemaManager->sanitize($schema, $insertQuery->getSource()),
-				]);
-			}
-
-			/**
-			 * @param IUpdateRelationQuery $updateRelationQuery
-			 *
-			 * @throws \Throwable
-			 */
-			protected function executeUpdateRelationQuery(IUpdateRelationQuery $updateRelationQuery) {
-				$relation = $updateRelationQuery->getRelation();
-				$cypher = 'MATCH';
-				$cypher .= "\n\t(a:" . $this->delimite(($sourceLink = $relation->getSourceLink())->getTargetSchema()->getRealName()) . ')';
-				$cypher .= '-[r:' . $this->delimite($relation->getSchema()->getRealName()) . ']->';
-				$cypher .= '(b:' . $this->delimite(($targetLink = $relation->getTargetLink())->getTargetSchema()->getRealName()) . ")\n";
-				$cypher .= 'WHERE';
-				$cypher .= "\n\ta." . ($source = $sourceLink->getTargetProperty()->getName()) . " = \$a AND";
-				$cypher .= "\n\tb." . ($target = $targetLink->getTargetProperty()->getName()) . " = \$b\n";
-				$cypher .= 'DELETE r';
-				$this->native($cypher, [
-					'a' => $updateRelationQuery->getFrom()[$source],
-					'b' => $updateRelationQuery->getTo()[$target],
-				]);
-				$this->executeCreateRelationQuery($updateRelationQuery);
-			}
-
-			/**
-			 * @param IUpdateLinkQuery $updateLinkQuery
-			 *
-			 * @throws \Throwable
-			 */
-			protected function executeUpdateLinkQuery(IUpdateLinkQuery $updateLinkQuery) {
-				$link = $updateLinkQuery->getLink();
-				$cypher = 'MATCH';
-				$cypher .= "\n\t(a:" . ($sourceName = $this->delimite(($sourceSchema = $link->getSourceSchema())->getRealName())) . ')';
-				$cypher .= '-[r:' . ($targetName = $this->delimite($link->getTargetSchema()->getRealName())) . ']->(:' . $targetName . ')';
-				$cypher .= 'WHERE';
-				$cypher .= "\n\ta." . $this->delimite($primary = $sourceSchema->getPrimary()->getName()) . " = \$a\n";
-				$cypher .= 'DELETE r';
-				$this->native($cypher, $parameters = [
-					'a' => $updateLinkQuery->getFrom()[$primary],
-					'b' => $updateLinkQuery->getTo()[$target = $link->getTargetProperty()->getName()],
-				]);
-				$cypher = 'MATCH';
-				$cypher .= "\n\t(a:" . $sourceName . '),';
-				$cypher .= "\n\t(b:" . $targetName . ")\n";
-				$cypher .= 'WHERE';
-				$cypher .= "\n\ta." . $this->delimite($primary) . " = \$a AND\n";
-				$cypher .= "\n\tb." . $this->delimite($target) . " = \$b\n";
-				$cypher .= "MERGE\n\t(a)-[:" . $targetName . ']->(b)';
-				$this->native($cypher, $parameters);
-			}
-
-			/**
-			 * @param ICreateRelationQuery $createRelationQuery
-			 *
-			 * @throws \Throwable
-			 */
-			protected function executeCreateRelationQuery(ICreateRelationQuery $createRelationQuery) {
-				$relation = $createRelationQuery->getRelation();
-				$cypher = 'MATCH';
-				$cypher .= "\n\t(a:" . $this->delimite(($sourceLink = $relation->getSourceLink())->getTargetSchema()->getRealName()) . '),';
-				$cypher .= "\n\t(b:" . $this->delimite(($targetLink = $relation->getTargetLink())->getTargetSchema()->getRealName()) . ")\n";
-				$cypher .= 'WHERE';
-				$cypher .= "\n\ta." . ($source = $sourceLink->getTargetProperty()->getName()) . " = \$a AND";
-				$cypher .= "\n\tb." . ($target = $targetLink->getTargetProperty()->getName()) . " = \$b\n";
-				$cypher .= "MERGE\n\t(a)-[:" . $this->delimite($relation->getSchema()->getRealName());
-				$properties = [
-					'a' => $createRelationQuery->getFrom()[$source],
-					'b' => $createRelationQuery->getTo()[$target],
-				];
-				if ($createRelationQuery->hasSource()) {
-					$cypher .= ' {';
-					$propertyList = [];
-					foreach ($createRelationQuery->getSource() as $k => $v) {
-						if ($v !== null) {
-							$propertyList[] = $this->delimite($k) . ': $' . ($parameterId = ('p_' . sha1(random_bytes(42))));
-							$properties[$parameterId] = $v;
-						}
-					}
-					$cypher .= implode(', ', $propertyList) . '}';
-				}
-				$cypher .= ']->(b)';
-				$this->native($cypher, $properties);
-			}
-
 			/**
 			 * @param ISelectQuery $selectQuery
 			 *
@@ -347,24 +218,6 @@ CYPHER;
 					$cypher .= "ORDER BY\n\t" . implode("\n\t,", $orderList);
 				}
 				return $this->native($cypher, $parameterList);
-			}
-
-			/**
-			 * @param IUpdateQuery $updateQuery
-			 *
-			 * @throws \Throwable
-			 */
-			protected function executeUpdateQuery(IUpdateQuery $updateQuery) {
-				$table = $updateQuery->getTable();
-				$cypher = "MATCH\n\t(" . ($alias = $this->delimite($table->getAlias())) . ':' . $this->delimite(($schema = $table->getSchema())->getRealName()) . ")\n";
-				$parameterList = [];
-				if ($table->hasWhere()) {
-					$cypher .= 'WHERE' . ($query = $this->fragmentWhereGroup($table->where()))->getQuery() . "\n";
-					$parameterList = $query->getParameterList();
-				}
-				$this->native($cypher . "SET\n\t" . $alias . ' = $set', array_merge($parameterList, [
-					'set' => $this->schemaManager->sanitize($schema, $updateQuery->getSource()),
-				]));
 			}
 
 			/**
