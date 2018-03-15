2075045c0730938fdc1c73ba61cb9cfe72aba20b
Marek Hanzal <marek.hanzal@x32.cz>
2018-03-15 22:40:52 +0100

    Common: Reworked common stuff to move dev. stuff into docker
    - tests are not enabled yet

---
+++ .docker/start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+set -e
+
+echo "Waiting for Neo4j"
+while ! timeout bash -c "echo > /dev/tcp/neo4j/7687"; do sleep 6; done > /dev/null
+echo "Waiting for MySQL"
+while ! timeout bash -c "echo > /dev/tcp/mysql/3306"; do sleep 6; done > /dev/null
+echo "Waiting for PostgreSQL"
+while ! timeout bash -c "echo > /dev/tcp/postgres/5432"; do sleep 6; done > /dev/null
+
+composer update --prefer-dist
+
+ssh-keygen -A
+/usr/sbin/sshd
+/usr/sbin/php-fpm7
+/usr/sbin/nginx
+tail -f /dev/null
