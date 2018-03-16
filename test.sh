51514d70441364266bc0978215387f1a53ee1d7a
Marek Hanzal <marek.hanzal@x32.cz>
2018-03-16 16:18:16 +0100

    Common: Coverage?

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Waiting for Neo4j"
-while ! timeout bash -c "echo > /dev/tcp/neo4j/7687 > /den/null"; do sleep 6; done
+while ! timeout bash -c "echo > /dev/tcp/neo4j/7687"; do sleep 6; done
 echo "Waiting for MySQL"
 while ! timeout bash -c "echo > /dev/tcp/mysql/3306"; do sleep 6; done
 echo "Waiting for PostgreSQL"
