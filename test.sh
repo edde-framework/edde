d85f1f9db12691d4944927f273ce996f5758b569
Marek Hanzal <marek.hanzal@x32.cz>
2018-03-16 16:16:52 +0100

    Common: Coverage?

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "Waiting for Neo4j"
-while ! timeout bash -c "echo > /dev/tcp/neo4j/7687"; do sleep 6; done > /dev/null
+while ! timeout bash -c "echo > /dev/tcp/neo4j/7687 > /den/null"; do sleep 6; done
 echo "Waiting for MySQL"
-while ! timeout bash -c "echo > /dev/tcp/mysql/3306"; do sleep 6; done > /dev/null
+while ! timeout bash -c "echo > /dev/tcp/mysql/3306"; do sleep 6; done
 echo "Waiting for PostgreSQL"
-while ! timeout bash -c "echo > /dev/tcp/postgres/5432"; do sleep 6; done > /dev/null
+while ! timeout bash -c "echo > /dev/tcp/postgres/5432"; do sleep 6; done
 
 lib/bin/phpunit
