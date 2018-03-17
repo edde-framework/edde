d7c2cec1ac6d03ef7accb30e52654d200bcb9461
Marek Hanzal <marek.hanzal@x32.cz>
2018-03-17 13:09:59 +0100

    Tests: Wanna more and more tests!

--- tests/src/Edde/Utils/StringUtilsTest.php
+++ tests/src/Edde/Utils/StringUtilsTest.php
@@ -14,4 +14,23 @@
 				'value' => 'bar',
 			], $this->stringUtils->match('foo=bar', '~(?<key>[a-z]+)=(?<value>[a-z]+)~', true));
 		}
+
+		public function testCapitalize() {
+			self::assertSame('Foo Bar', $this->stringUtils->capitalize('foo bar'));
+		}
+
+		public function testFromCamelCase() {
+			self::assertEquals([
+				'Foo',
+				'Bar',
+			], $this->stringUtils->fromCamelCase('FooBar'));
+			self::assertEquals([
+				'Bar',
+			], $this->stringUtils->fromCamelCase('FooBar', 1));
+		}
+
+		public function testRecamel() {
+			self::assertEquals('foo-bar', $this->stringUtils->recamel('FooBar'));
+			self::assertEquals('bar#prd', $this->stringUtils->recamel('FooBarPrd', '#', 1));
+		}
 	}
