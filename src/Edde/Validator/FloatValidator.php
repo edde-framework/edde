f4ed5b685970fbf385b3fc5e2add8d785b901ecf
Marek Hanzal <marek.hanzal@x32.cz>
2018-03-20 15:35:04 +0100

    Validator: Soften float validator to accept also ints

--- src/Edde/Validator/FloatValidator.php
+++ src/Edde/Validator/FloatValidator.php
@@ -2,10 +2,12 @@
 	declare(strict_types=1);
 	namespace Edde\Validator;
 
+	use function is_int;
+
 	class FloatValidator extends AbstractValidator {
 		/** @inheritdoc */
 		public function validate($value, array $options = []): void {
-			if (is_float($value) === false) {
+			if (is_float($value) === false || is_int($value) === false) {
 				throw new ValidationException(
 					vsprintf('Given value%s of type [%s] is not a float!', [
 						isset($options['::name']) ? ' [' . $options['::name'] . ']' : '',
