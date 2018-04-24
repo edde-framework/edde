335b1d7c3b6f25b9d490e49ac5db66ac62649890
Marek Hanzal <marek.hanzal@x32.cz>
2018-04-24 13:08:27 +0200

    Filter: Preparing for a filter upgrade

--- src/Edde/Filter/DateTimeFilter.php
+++ src/Edde/Filter/DateTimeFilter.php
@@ -2,11 +2,16 @@
 	declare(strict_types=1);
 	namespace Edde\Filter;
 
+	use DateTime;
+
 	class DateTimeFilter extends AbstractFilter {
-		/**
-		 * @inheritdoc
-		 */
+		/** @inheritdoc */
 		public function filter($value, array $options = []) {
-			return $value ? new \DateTime($value) : null;
+			if ($value instanceof DateTime) {
+				return $value;
+			} else if ($value) {
+				return new DateTime($value);
+			}
+			return null;
 		}
 	}
