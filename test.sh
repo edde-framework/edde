36cccb174362a2e05053dd20fb10aaf7542cc9a1
Marek Hanzal <marek.hanzal@x32.cz>
2018-06-22 12:31:36 +0200

    Docker: Fixed database hostnames

--- test.sh
+++ test.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-#echo "Waiting for Neo4j"
-#while ! timeout bash -c "echo > /dev/tcp/neo4j/7687"; do sleep 6; done
 echo "Waiting for MySQL"
-while ! timeout bash -c "echo > /dev/tcp/mysql/3306"; do sleep 6; done
+while ! timeout bash -c "echo > /dev/tcp/edde-mysql/3306"; do sleep 6; done
 echo "Waiting for PostgreSQL"
-while ! timeout bash -c "echo > /dev/tcp/postgres/5432"; do sleep 6; done
+while ! timeout bash -c "echo > /dev/tcp/edde-postgres/5432"; do sleep 6; done
 
 lib/bin/phpunit --coverage-text
