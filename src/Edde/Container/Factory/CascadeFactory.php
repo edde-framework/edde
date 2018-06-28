d84baed219c33edc696d6d3b51050907151b199c
Marek Hanzal <marek.hanzal@x32.cz>
2018-06-28 13:09:23 +0200

    Application: Removed obsolete Context
    - updated and simplified CascadeFactory

--- src/Edde/Container/Factory/CascadeFactory.php
+++ src/Edde/Container/Factory/CascadeFactory.php
@@ -2,14 +2,62 @@
 	declare(strict_types=1);
 	namespace Edde\Container\Factory;
 
-	use Edde\Application\IContext;
 	use Edde\Container\IContainer;
+	use Edde\Container\IReflection;
 
-	class CascadeFactory extends AbstractDiscoveryFactory {
+	class CascadeFactory extends ClassFactory {
+		/** @var string[] */
+		protected $cascades = [];
+		/** @var string[] */
+		protected $names = [];
+
+		/**
+		 * @param string[] $cascades
+		 */
+		public function __construct(array $cascades = ['Edde']) {
+			$this->cascades = $cascades;
+		}
+
 		/** @inheritdoc */
-		protected function discover(IContainer $container, string $name): array {
-			/** @var $context IContext */
-			$context = $container->create(IContext::class, [], __METHOD__);
-			return $context->cascade('\\', $name);
+		public function canHandle(IContainer $container, string $dependency): bool {
+			if ($name = $this->search($dependency)) {
+				return parent::canHandle($container, $name);
+			}
+			return false;
+		}
+
+		/** @inheritdoc */
+		public function getReflection(IContainer $container, string $dependency): IReflection {
+			return parent::getReflection($container, $this->search($dependency));
+		}
+
+		/** @inheritdoc */
+		public function factory(IContainer $container, array $params, IReflection $dependency, string $name = null) {
+			return parent::factory($container, $params, $dependency, $this->search($name));
+		}
+
+		/** @inheritdoc */
+		protected function search(string $name = null) {
+			if (isset($this->names[$name]) || array_key_exists($name, $this->names)) {
+				return $this->names[$name];
+			}
+			foreach ($this->discover($name) as $source) {
+				if (class_exists($source)) {
+					return $this->names[$name] = $source;
+				}
+			}
+			return $this->names[$name] = null;
+		}
+
+		/** @inheritdoc */
+		protected function discover(string $name = null): array {
+			if ($name === null) {
+				return $this->cascades;
+			}
+			$names = [];
+			foreach ($this->cascades as $cascade) {
+				$names[] = $cascade . '\\' . $name;
+			}
+			return $names;
 		}
 	}
--- tests/src/Edde/Application/assets.php
+++ tests/src/Edde/Application/assets.php
@@ -4,16 +4,6 @@
 
 	use Edde\Edde;
 	use Edde\Router\AbstractRouter;
-	use function array_merge;
-
-	class TestContext extends AbstractContext {
-		public function cascade(string $delimiter, string $name = null): array {
-			return array_merge(parent::cascade($delimiter, $name), [
-				'Foo' . $delimiter . 'Bar' . ($name ? $delimiter . $name : ''),
-				'Bar' . $delimiter . 'Foo' . ($name ? $delimiter . $name : ''),
-			]);
-		}
-	}
 
 	class TestService extends Edde implements IController {
 		public function noResponse() {
