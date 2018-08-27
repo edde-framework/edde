2cf32561437bc8c0b3b2ee6889ee1571ca25467f
Marek Hanzal <marek.hanzal@x32.cz>
2018-08-27 16:22:41 +0200

    Docker: WTF!?!?

--- test.sh
+++ test.sh
@@ -2,15 +2,15 @@
 set -e
 
 echo "Waiting for MySQL"
-while !(nc -z edde-mysql 3306); do
-	echo -n .
-	sleep 1
-done
+#while !(nc -z edde-mysql 3306); do
+#	echo -n .
+#	sleep 1
+#done
 echo "Waiting for PostgreSQL"
-while !(nc -z edde-postgres 5432); do
-	echo -n .
-	sleep 1
-done
+#while !(nc -z edde-postgres 5432); do
+#	echo -n .
+#	sleep 1
+#done
 
 echo "YAHOOO"
 
