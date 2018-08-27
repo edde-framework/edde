74cff035a8e69ee71144d96c874709a34c11f776
Marek Hanzal <marek.hanzal@x32.cz>
2018-08-27 16:19:23 +0200

    Docker: Added just synthetic wait

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "Waiting for MySQL"
-while ! timeout bash -c "echo > /dev/tcp/edde-mysql/3306"; do sleep 1; done
+#while ! timeout bash -c "echo > /dev/tcp/edde-mysql/3306"; do sleep 1; done
 sleep 1
 echo "Waiting for Postgres"
-while ! timeout bash -c "echo > /dev/tcp/edde-postgres/5432"; do sleep 1; done
-sleep 1
+#while ! timeout bash -c "echo > /dev/tcp/edde-postgres/5432"; do sleep 1; done
+sleep 6
 
 phpunit --coverage-text
