43f890ad31145c5c5aa4fca2097ca5cc63b21d26
Marek Hanzal <marek.hanzal@x32.cz>
2018-08-27 17:09:21 +0200

    Docker: So, the bash shit is not working, but PHP rulezz!

--- test.sh
+++ test.sh
@@ -1,20 +1,30 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Waiting for MySQL"
-#while !(nc -z edde-mysql 3306); do
-	echo -n .
-	echo -n .
-	sleep 3
-	echo -n .
-	sleep 1
-#done
-echo "Waiting for PostgreSQL"
-#while !(nc -z edde-postgres 5432); do
-#	echo -n .
-#	sleep 1
-#done
-
-echo "YAHOOO"
-
-#phpunit --coverage-text
+#!/usr/bin/env php
+<?php
+	declare(strict_types=1);
+	$ports = [
+		3306 => [
+			'desc' => 'MySQL',
+			'host' => 'edde-mysql',
+		],
+		5432 => [
+			'desc' => 'Postgres',
+			'host' => 'edde-postgres',
+		],
+	];
+	$attempts = 8;
+	$sleep = 1;
+	foreach ($ports as $port => $item) {
+		printf("Waiting for [%s] on [%s:%s]\n", $item['desc'], $item['host'], $port);
+		for ($i = 0; $i < $attempts; $i++) {
+			if (($connection = @fsockopen($item['host'], $port)) === false) {
+				printf(".");
+				sleep($sleep);
+				continue;
+			}
+			fclose($connection);
+			printf("\tService available!\n\n");
+			continue 2;
+		}
+		die(sprintf("\nFailed to wait for a service [%s].\n", $item['desc']));
+	}
+	echo shell_exec('phpunit --coverage-text');
