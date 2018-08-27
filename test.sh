b0cd9abb81d1d378a6c7877afdb372deabcc3b8a
Marek Hanzal <marek.hanzal@x32.cz>
2018-08-27 16:15:10 +0200

    Docker: Trying to make the shit work; still dunno what's breaking the things

--- test.sh
+++ test.sh
@@ -3,7 +3,9 @@ set -e
 
 echo "Waiting for MySQL"
 while ! timeout bash -c "echo > /dev/tcp/edde-mysql/3306"; do sleep 1; done
+sleep 1
 echo "Waiting for Postgres"
 while ! timeout bash -c "echo > /dev/tcp/edde-postgres/5432"; do sleep 1; done
+sleep 1
 
 phpunit --coverage-text
