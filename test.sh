963ef94b053d55517e8f1f3d78c0935225087d3c
Marek Hanzal <marek.hanzal@x32.cz>
2018-08-27 16:24:35 +0200

    Docker: So what's the fucking problem there?!

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "Waiting for MySQL"
-#while !(nc -z edde-mysql 3306); do
-#	echo -n .
+while !(nc -z edde-mysql 3306); do
+	echo -n .
 #	sleep 1
-#done
+done
 echo "Waiting for PostgreSQL"
 #while !(nc -z edde-postgres 5432); do
 #	echo -n .
