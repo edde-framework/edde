c85c216e484335a5bcf6967f13272305c5c6c6e0
Marek Hanzal <marek.hanzal@x32.cz>
2018-12-21 15:37:14 +0100

    Common: Docs in message queue schema

--- src/Edde/Message/MessageQueueSchema.php
+++ src/Edde/Message/MessageQueueSchema.php
@@ -6,11 +6,29 @@
 	use Edde\Schema\UuidSchema;
 
 	interface MessageQueueSchema extends UuidSchema {
+		/**
+		 * when a message should be executed (it could be event later this time)
+		 */
 		public function stamp(): DateTime;
 
+		/**
+		 * actual planned execution time - message should not be processed without
+		 * this time set
+		 */
+		public function executeOn(): ?DateTime;
+
+		/**
+		 * message type from specification
+		 */
 		public function type(): string;
 
+		/**
+		 * optional message target from specification
+		 */
 		public function target(): ?string;
 
+		/**
+		 * message itself
+		 */
 		public function message($type = 'json');
 	}
