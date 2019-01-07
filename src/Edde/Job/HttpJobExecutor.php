2f325d0800b480df71cf6ebb4251e5e7eb30b7e2
Marek Hanzal <marek.hanzal@x32.cz>
2019-01-07 12:29:20 +0100

    Common: Improved http executor

--- src/Edde/Job/HttpJobExecutor.php
+++ src/Edde/Job/HttpJobExecutor.php
@@ -5,27 +5,37 @@
 	use Edde\Edde;
 	use Edde\Message\IPacket;
 	use Edde\Service\Config\ConfigService;
+	use function fwrite;
 	use function json_encode;
+	use function parse_url;
+	use function strlen;
+	use function vsprintf;
 
 	class HttpJobExecutor extends Edde implements IJobExecutor {
 		use ConfigService;
 		protected $url;
+		/** @var array */
+		protected $parts;
 
 		/** @inheritdoc */
 		public function execute(IPacket $packet): IJobExecutor {
-			file_get_contents($this->url, false, stream_context_create([
-				'http' => [
-					'header'  => "Content-type: application/json\r\n",
-					'method'  => 'POST',
-					'content' => json_encode($packet->export()),
-				],
+			$content = json_encode($packet->export());
+			if (($socket = fsockopen($this->parts['host'], isset($this->parts['port']) ? $this->parts['port'] : 80, $status, $error, 15)) === false) {
+				throw new JobException(sprintf('Cannot connect to [%s].', $this->url));
+			}
+			fwrite($socket, vsprintf("POST %s HTTP/1.1\r\nHost: %s\r\nContent-Type: application/json\r\nContent-Length: %d\r\nConnection: Close\r\n\r\n", [
+				$this->parts['path'],
+				$this->parts['host'],
+				strlen($content),
 			]));
+			fwrite($socket, $content);
+			fclose($socket);
 			return $this;
 		}
 
 		protected function handleInit(): void {
 			parent::handleInit();
 			$section = $this->configService->optional('http-job-executor');
-			$this->url = $section->optional('url', 'http://localhost/rest/message.bus');
+			$this->parts = parse_url($this->url = $section->optional('url', 'http://localhost/rest/message.bus'));
 		}
 	}
