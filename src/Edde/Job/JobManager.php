6a866b32f5d45709ade23afe99ef464302b6fabb
Marek Hanzal <marek.hanzal@x32.cz>
2019-01-07 11:35:55 +0100

    Job: Tuning up job manager

--- src/Edde/Job/JobManager.php
+++ src/Edde/Job/JobManager.php
@@ -4,62 +4,24 @@
 
 	use Edde\Edde;
 	use Edde\Service\Job\JobQueue;
-	use Edde\Storage\EmptyEntityException;
-	use function sprintf;
 
 	class JobManager extends Edde implements IJobManager {
 		use JobQueue;
 
 		/** @inheritdoc */
 		public function run(): IJobManager {
-			$this->startup();
-			while ($this->running) {
+			while (true) {
 				$this->tick();
 				/**
 				 * heartbeat rate to keep stuff on rails
 				 */
-				usleep($this->rate * 1000);
+				usleep(750 * 1000);
 			}
-			$this->shutdown();
 			return $this;
 		}
 
 		/** @inheritdoc */
 		public function tick(): IJobManager {
-			try {
-				$job = $this->jobQueue->enqueue();
-				/**
-				 * limit concurrency level
-				 */
-				if (count($this->pids) < $this->limit) {
-					if (($pid = $this->pids[] = pcntl_fork()) === 0) {
-						if (pcntl_exec($this->binary, [
-								$this->controller,
-								'--' . $this->param . '=' . $job['uuid'],
-							]) === false) {
-							throw new JobException(sprintf('Cannot execute a job [%s].', $job['uuid']));
-						}
-					} else if ($pid === -1) {
-						throw new JobException(sprintf('Cannot fork a job [%s].', $job['uuid']));
-					}
-				}
-			} catch (EmptyEntityException $exception) {
-				/**
-				 * noop
-				 */
-			}
-			/**
-			 * pickup children process to prevent zombies
-			 */
-			pcntl_waitpid(0, $status, WNOHANG);
-			/**
-			 * remove already executed PIDs
-			 */
-			foreach ($this->pids as $i => $pid) {
-				if (posix_getpgid($pid) === false) {
-					unset($this->pids[$i]);
-				}
-			}
 			return $this;
 		}
 	}
