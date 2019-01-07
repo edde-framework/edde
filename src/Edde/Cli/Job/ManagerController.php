684b8bc710557dd9cea4cb038b3bb4329ab7beac
Marek Hanzal <marek.hanzal@x32.cz>
2019-01-07 12:11:14 +0100

    Common: Cleaned up manager controller

--- src/Edde/Cli/Job/ManagerController.php
+++ src/Edde/Cli/Job/ManagerController.php
@@ -5,24 +5,15 @@
 	use Edde\Controller\CliController;
 	use Edde\Service\Job\JobManager;
 	use Edde\Service\Job\JobQueue;
-	use Edde\Service\Log\LogService;
 
 	class ManagerController extends CliController {
-		use LogService;
 		use JobQueue;
 		use JobManager;
-		protected $running = true;
 
 		public function actionRun(): void {
 			$this->printf('Job: Running endless loop');
 			$this->jobManager->run();
 			$this->printf('Job: Finished');
-		}
-
-		public function actionExecute(): void {
-			$this->printf('Executing job [%s]', $uuid = $this->getParams()['job']);
-			$this->jobQueue->execute($uuid);
-			$this->printf('Done job [%s]', $uuid);
 		}
 
 		public function actionCleanup(): void {
