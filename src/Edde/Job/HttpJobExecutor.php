b767d2114af13b811724e9c5ba12b2337c3f1403
Marek Hanzal <marek.hanzal@x32.cz>
2019-01-08 18:14:46 +0100

    Job: Removed reset flag as it has currently no use

--- src/Edde/Job/HttpJobExecutor.php
+++ src/Edde/Job/HttpJobExecutor.php
@@ -20,8 +20,7 @@
 			$this->storage->transaction(function () use ($job) {
 				$job = $this->jobQueue->state($job, JobSchema::STATE_SCHEDULED);
 				$url = Url::create($this->url)->setParams([
-					'job'   => $job['uuid'],
-					'reset' => $job['state'] === JobSchema::STATE_RESET,
+					'job' => $job['uuid'],
 				]);
 				if (($socket = fsockopen($url->getHost(), $url->getPort(80), $status, $error, 15)) === false) {
 					throw new JobException(sprintf('Cannot connect to [%s].', $this->url));
